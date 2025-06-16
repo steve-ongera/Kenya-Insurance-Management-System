e2d3edc0ea5af2a113e52c01ff0b111e10025be3
steve <gadafisteve001@gmail.com>
2025-06-16 18:17:47 +0300

    updates

--- insurance_system/settings.py
+++ insurance_system/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'insurance_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mywebsite/views.py
+++ mywebsite/views.py
@@ -27,7 +27,7 @@ def login_view(request):
     else:
         form = LoginForm()
 
-    return render(request, 'accounts/login.html', {'form': form})
+    return render(request, 'auth/login.html', {'form': form})
 
 @login_required
 def logout_view(request):
